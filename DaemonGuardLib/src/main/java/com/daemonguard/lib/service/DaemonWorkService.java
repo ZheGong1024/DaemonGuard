46e20e35989c52e00ff41fee72cc5cda3349fc9f
ZheGong <zgong@bbmtek.com>
2018-03-29 14:35:05 -0400

    Refactor daemon work service.

--- DaemonGuardLib/src/main/java/com/daemonguard/lib/service/DaemonWorkService.java
+++ DaemonGuardLib/src/main/java/com/daemonguard/lib/service/DaemonWorkService.java
@@ -14,6 +14,7 @@ import com.daemonguard.lib.WakeUpReceiver;
 
 public abstract class DaemonWorkService extends Service {
   protected boolean mFirstStarted = true;
+  private boolean shouldStopService = false;
 
   /**
    * 用于在不需要服务运行的时候取消 Job / Alarm / Subscription.
@@ -29,22 +30,11 @@ public abstract class DaemonWorkService extends Service {
    *
    * @return 应当停止服务, true; 应当启动服务, false; 无法判断, 什么也不做, null.
    */
-  public abstract Boolean shouldStopService(Intent intent, int flags, int startId);
-
-  public abstract void startWork(Intent intent, int flags, int startId);
-
-  public abstract void stopWork(Intent intent, int flags, int startId);
-
-  /**
-   * 任务是否正在运行?
-   *
-   * @return 任务正在运行, true; 任务当前不在运行, false; 无法判断, 什么也不做, null.
-   */
-  public abstract Boolean isWorkRunning(Intent intent, int flags, int startId);
+  public boolean shouldStopService() {
+    return shouldStopService;
+  }
 
   @Nullable public abstract IBinder onBind(Intent intent, Void alwaysNull);
-
-  public abstract void onServiceKilled(Intent rootIntent);
 
   /**
    * 1.防止重复启动，可以任意调用 DaemonEnv.startServiceMayBind(Class serviceClass);
@@ -53,19 +43,14 @@ public abstract class DaemonWorkService extends Service {
    * 4.启动守护服务;
    * 5.守护 Service 组件的启用状态, 使其不被 MAT 等工具禁用.
    */
-  protected int onStart(Intent intent, int flags, int startId) {
-
+  protected int onStart() {
     //启动守护服务，运行在:watch子进程中
     Daemon.getInstance().startServiceMayBind(DaemonService.class);
 
     //业务逻辑: 实际使用时，根据需求，将这里更改为自定义的条件，判定服务应当启动还是停止 (任务是否需要运行)
-    Boolean shouldStopService = shouldStopService(intent, flags, startId);
-    if (shouldStopService != null) {
-      if (shouldStopService) {
-        stopService(intent, flags, startId);
-      } else {
-        startService(intent, flags, startId);
-      }
+    boolean shouldStopService = shouldStopService();
+    if (shouldStopService) {
+      stopDaemon();
     }
 
     if (mFirstStarted) {
@@ -89,43 +74,30 @@ public abstract class DaemonWorkService extends Service {
     return START_STICKY;
   }
 
-  void startService(Intent intent, int flags, int startId) {
-    //检查服务是否不需要运行
-    Boolean shouldStopService = shouldStopService(intent, flags, startId);
-    if (shouldStopService != null && shouldStopService) return;
-    //若还没有取消订阅，说明任务仍在运行，为防止重复启动，直接 return
-    Boolean workRunning = isWorkRunning(intent, flags, startId);
-    if (workRunning != null && workRunning) return;
-    //业务逻辑
-    startWork(intent, flags, startId);
-  }
-
   /**
    * 停止服务并取消定时唤醒
    *
-   * 停止服务使用取消订阅的方式实现，而不是调用 Context.stopService(Intent name)。因为：
-   * 1.stopService 会调用 Service.onDestroy()，而 AbsWorkService 做了保活处理，会把 Service 再拉起来；
+   * 停止服务使用取消订阅的方式实现，而不是调用 Context.stopDaemon(Intent name)。因为：
+   * 1.stopDaemon 会调用 Service.onDestroy()，而 AbsWorkService 做了保活处理，会把 Service 再拉起来；
    * 2.我们希望 AbsWorkService 起到一个类似于控制台的角色，即 AbsWorkService 始终运行 (无论任务是否需要运行)，
    * 而是通过 onStart() 里自定义的条件，来决定服务是否应当启动或停止。
    */
-  void stopService(Intent intent, int flags, int startId) {
-    //取消对任务的订阅
-    stopWork(intent, flags, startId);
+  public void stopDaemon() {
+    shouldStopService = true;
     //取消 Job / Alarm / Subscription
     cancelJobAlarmSub();
   }
 
   @Override public int onStartCommand(Intent intent, int flags, int startId) {
-    return onStart(intent, flags, startId);
+    return onStart();
   }
 
   @Nullable @Override public IBinder onBind(Intent intent) {
-    onStart(intent, 0, 0);
+    onStart();
     return onBind(intent, null);
   }
 
   protected void onEnd(Intent rootIntent) {
-    onServiceKilled(rootIntent);
     if (!Daemon.getInstance().isInitialized) return;
     Daemon.getInstance().startServiceMayBind(Daemon.getInstance().mWorkService);
     Daemon.getInstance().startServiceMayBind(DaemonService.class);
