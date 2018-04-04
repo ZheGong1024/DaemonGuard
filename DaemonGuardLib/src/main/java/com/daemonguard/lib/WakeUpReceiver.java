15248634f68ae8f40b2fa9550192c48806a7223d
ZheGong <zgong@bbmtek.com>
2018-04-04 13:11:00 -0400

    Update comments.

--- DaemonGuardLib/src/main/java/com/daemonguard/lib/WakeUpReceiver.java
+++ DaemonGuardLib/src/main/java/com/daemonguard/lib/WakeUpReceiver.java
@@ -8,12 +8,14 @@ import android.util.Log;
 public class WakeUpReceiver extends BroadcastReceiver {
 
   /**
-   * 监听 8 种系统广播 :
-   * CONNECTIVITY\_CHANGE, USER\_PRESENT, ACTION\_POWER\_CONNECTED, ACTION\_POWER\_DISCONNECTED,
-   * BOOT\_COMPLETED, MEDIA\_MOUNTED, PACKAGE\_ADDED, PACKAGE\_REMOVED.
-   * 在网络连接改变, 用户屏幕解锁, 电源连接 / 断开, 系统启动完成, 挂载 SD 卡, 安装 / 卸载软件包时拉起 Service.
-   * Service 内部做了判断，若 Service 已在运行，不会重复启动.
-   * 运行在:watch子进程中.
+   * Listening to the following events to start daemon job service:
+   * android.intent.action.USER_PRESENT: User unlock the screen.
+   * android.intent.action.ACTION_POWER_CONNECTED: Power connected.
+   * android.intent.action.ACTION_POWER_DISCONNECTED: Power disconnected.
+   * android.intent.action.BOOT_COMPLETED: System boot.
+   * android.net.conn.CONNECTIVITY_CHANGE: Connectivity changes.
+   * android.intent.action.PACKAGE_ADDED: App install.
+   * android.intent.action.PACKAGE_REMOVED: App uninstall.
    */
   @Override public void onReceive(Context context, Intent intent) {
     if (!Daemon.getInstance().isInitialized) return;
